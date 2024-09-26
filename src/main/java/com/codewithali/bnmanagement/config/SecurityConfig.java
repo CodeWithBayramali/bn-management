4739a65817f8658de56f0de06db91b08ba174220
CodeWithBayramali <bdurak3@gmail.com>
2024-09-26 19:07:53 +0300

    ssl-completed

--- src/main/java/com/codewithali/bnmanagement/config/SecurityConfig.java
+++ src/main/java/com/codewithali/bnmanagement/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://bn.org.tr", "https://bn.org.tr", "http://localhost:3000","https://localhost:3000")); // İzin verilen URL'ler
+        config.setAllowedOrigins(Arrays.asList("https://bn.org.tr")); // İzin verilen URL'ler
         config.setAllowedHeaders(Arrays.asList("*")); // İzin verilen başlıklar
         config.setAllowedMethods(Arrays.asList("*")); // İzin verilen HTTP metodları
         config.setAllowCredentials(true); // Kimlik bilgilerini kabul et
